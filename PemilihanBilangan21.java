b240ac207c1f8d979c2fd313e15fb492c6c3eb4d
naylaakas <naylakas05@gmail.com>
2024-09-26 08:01:39 +0700

    hasil modifikasi

--- PemilihanBilangan21.java
+++ PemilihanBilangan21.java
@@ -7,13 +7,8 @@ public class PemilihanBilangan21 {
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }
-        else
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        String hasil;
+        hasil = (angka % 2 == 0) ? "termasuk bilangan genap" : "termasuk bilangan ganjil";
+        System.out.println(angka + " adalah " + hasil);
     }
 }
